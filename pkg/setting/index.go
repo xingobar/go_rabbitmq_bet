f847fd5d3baeaf0e3388695582a2173a4ca52b07
xingobar <gary840227@gmail.com>
2020-09-13 15:58:47 +0800

    [refactor] code refactor

--- pkg/setting/index.go
+++ pkg/setting/index.go
@@ -1,14 +1,5 @@
 package setting
 
-// 彩票 queue map
-var LotteryQueueMap = map[string] string {
-	"1": "pk10",
-	"8": "pk10",
-}
-
-// 隊列名稱
-var QueueName = []string {"pk10"}
-
 // 取得彩種 map
 func GetQueueNameByLottery(key string) string {
 	val, ok := LotteryQueueMap[key]
--- pkg/setting/variables.go
+++ pkg/setting/variables.go
@@ -2,3 +2,12 @@ package setting
 
 // 每頁幾筆
 const PER_PAGE = 20
+
+// 彩票 queue map
+var LotteryQueueMap = map[string] string {
+	"1": "pk10",
+	"8": "pk10",
+}
+
+// 隊列名稱
+var QueueName = []string {"pk10"}
